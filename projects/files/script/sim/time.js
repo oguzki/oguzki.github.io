70de86b49128e4e2cdced1c948b6ba1b0a263c5d
qnezor <qnezoru@gmail.com>
2023-05-30 16:50:20 +0300

    1

---
+++ projects/files/script/sim/time.js
@@ -0,0 +1,49 @@
+function nextDay() {
+    day = day + 1;
+    randomsob();
+    if (mon === 2) {
+        if (day === 29) {
+            nextMon();
+            stp();
+        }
+    }
+    if (day === 32) {
+        nextMon();
+        stp();
+    } else {
+        daychange();
+    }
+
+    if (mon === 13) {
+        nextYear();
+    }
+}
+
+function nextMon() {
+    mon = mon + 1;
+    day = 1;
+    monchange();
+    daychange();
+}
+function nextYear() {
+    year = year + 1;
+    mon = 1;
+    monchange();
+    yearchange();
+}
+
+function daychange() {
+    document.getElementById('dayD').innerHTML = day;
+}
+
+function monchange() {
+    document.getElementById('monD').innerHTML = mon;
+}
+
+function yearchange() {
+    document.getElementById('yearD').innerHTML = year;
+}
+
+function moneychange() {
+    document.getElementById('dengi').innerHTML = money;
+}
